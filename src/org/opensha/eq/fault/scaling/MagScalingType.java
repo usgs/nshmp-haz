9cba382c6141933f4d6bd1d0a52a09b7a98b24de
Peter Powers <pmpowers@usgs.gov>
2014-03-28 01:14:51 -0600

    added magScaling enum

---
+++ src/org/opensha/eq/fault/scaling/MagScalingType.java
@@ -0,0 +1,13 @@
+package org.opensha.eq.fault.scaling;
+
+/**
+ * Magnitude scaling realtionship identifiers.
+ * 
+ * @author Peter Powers
+ */
+public enum MagScalingType {
+
+	NSHMP_CA,
+	WC_94_LENGTH;
+	
+}
