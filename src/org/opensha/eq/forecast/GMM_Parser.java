3ccd8f07dd5bd473d2e3dfac0362b0ff24e350c8
Peter Powers <pmpowers@usgs.gov>
2014-05-21 10:59:02 -0600

    relocated GMM_Parser to forecast pkg

--- src/org/opensha/eq/forecast/GMM_Parser.java
+++ src/org/opensha/eq/forecast/GMM_Parser.java
@@ -1,4 +1,4 @@
-package org.opensha.calc;
+package org.opensha.eq.forecast;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
