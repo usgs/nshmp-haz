8c6fb53fb88dd275da4d94873169678d39dd38fc
Peter Powers <pmpowers@usgs.gov>
2014-11-10 11:03:06 -0700

    area sources

---
+++ src/org/opensha/eq/model/AreaSourceSet.java
@@ -0,0 +1,104 @@
+package org.opensha.eq.model;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
+import static org.opensha.data.DataUtils.validateWeight;
+import static org.opensha.util.TextUtils.validateName;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.opensha.eq.fault.scaling.MagScalingType;
+import org.opensha.geo.Location;
+
+import com.google.common.base.Predicate;
+import com.google.common.collect.Lists;
+
+/**
+ * A container class for related {@link AreaSource}s.
+ * 
+ * @author Peter Powers
+ */
+public class AreaSourceSet extends AbstractSourceSet<AreaSource> {
+
+	final private List<AreaSource> sources;
+
+	private AreaSourceSet(String name, double weight, MagScalingType msrType,
+		List<AreaSource> sources, GmmSet gmmSet) {
+		super(name, weight, msrType, gmmSet);
+		this.sources = sources;
+	}
+
+	@Override public Iterator<AreaSource> iterator() {
+		return sources.iterator();
+	}
+
+	@Override public int size() {
+		return sources.size();
+	}
+
+	@Override public SourceType type() {
+		return SourceType.AREA;
+	}
+
+	@Override public Predicate<AreaSource> distanceFilter(final Location loc, final double distance) {
+		return new Predicate<AreaSource>() {
+			@Override public boolean apply(AreaSource source) {
+				return source.border().minDistToLocation(loc) <= distance;
+			}
+		};
+	}
+
+	/* Single use builder. */
+	static class Builder {
+
+		static final String ID = "AreaSourceSet.Builder";
+		boolean built = false;
+
+		String name;
+		Double weight;
+		MagScalingType magScaling;
+		GmmSet gmmSet;
+		List<AreaSource> sources = Lists.newArrayList();
+
+		Builder name(String name) {
+			this.name = validateName(name);
+			return this;
+		}
+
+		Builder weight(double weight) {
+			this.weight = validateWeight(weight);
+			return this;
+		}
+
+		Builder gmms(GmmSet gmmSet) {
+			this.gmmSet = checkNotNull(gmmSet);
+			return this;
+		}
+
+		Builder magScaling(MagScalingType magScaling) {
+			this.magScaling = checkNotNull(magScaling, "MagScalingType is null");
+			return this;
+		}
+
+		Builder source(AreaSource source) {
+			sources.add(checkNotNull(source, "AreaSource is null"));
+			return this;
+		}
+
+		void validateState(String id) {
+			checkState(!built, "This %s instance as already been used", id);
+			checkState(name != null, "%s name not set", id);
+			checkState(weight != null, "%s weight not set", id);
+			checkState(magScaling != null, "%s mag-scaling relation not set", id);
+			checkState(gmmSet != null, "%s ground motion models not set", id);
+			built = true;
+		}
+
+		AreaSourceSet build() {
+			validateState(ID);
+			return new AreaSourceSet(name, weight, magScaling, sources, gmmSet);
+		}
+	}
+
+}
