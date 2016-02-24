c9211b51a9a8496aac20c1430d9b4b5a5f6c6cec
Peter Powers <pmpowers@usgs.gov>
2016-02-24 10:07:17 -0700

    updated package-info comments

--- src/org/opensha2/geo/package-info.java
+++ src/org/opensha2/geo/package-info.java
@@ -1,4 +1,7 @@
 /**
  * Classes and utilities for working with geographic data.
+ * 
+ * <p>All utility methods in this package assume that depths are positive-down,
+ * consistent with seismological convention.
  */
 package org.opensha2.geo;
