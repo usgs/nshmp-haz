6e36eeab42bf4744a8c5c0d8adb8a03045ae2734
Peter Powers <pmpowers@usgs.gov>
2016-12-05 13:42:30 -0700

    cybershake sites and export util

--- src/org/opensha2/internal/NshmpSiteFiles.java
+++ src/org/opensha2/internal/NshmpSiteFiles.java
@@ -19,6 +19,7 @@ import org.opensha2.geo.Location;
 import org.opensha2.geo.LocationList;
 import org.opensha2.internal.GeoJson.Feature;
 import org.opensha2.internal.GeoJson.FeatureCollection;
+import org.opensha2.internal.GeoJson.PointProperties;
 import org.opensha2.util.NamedLocation;
 
 import com.google.common.base.Function;
@@ -63,6 +64,9 @@ final class NshmpSiteFiles {
    */
   public static void main(String[] args) throws IOException {
     writeNshmpSites();
+    writeSites("nureg", EnumSet.allOf(NuregSite.class), DEC3_FMT);
+    writeCybershakeSites("cybershake", EnumSet.allOf(CybershakeSite.class));
+
     // writeNshmpPolys();
     // writeNshmpSummaryPoly();
     // writeNshmpSites_0p1();
@@ -204,14 +208,18 @@ final class NshmpSiteFiles {
   }
 
   static void writeNshmpSites_0p1() throws IOException {
-    writeSites("ceus-0p1",
+    writeSites(
+        "ceus-0p1",
         FluentIterable.from(NshmpSite.ceus())
             .transform(adjustLocation_0p1())
-            .toList());
-    writeSites("wus-0p1",
+            .toList(),
+        DEC2_FMT);
+    writeSites(
+        "wus-0p1",
         FluentIterable.from(NshmpSite.wus())
             .transform(adjustLocation_0p1())
-            .toList());
+            .toList(),
+        DEC2_FMT);
   }
 
   static void writeNshmpSites() throws IOException {
@@ -219,39 +227,74 @@ final class NshmpSiteFiles {
     writeNshmpSites("ceus", NshmpSite.ceus());
     writeNshmpSites("wus", NshmpSite.wus());
     writeNshmpSites("nrc", NshmpSite.nrc());
-    writeSites("nehrp", NshmpSite.nehrp());
+    writeSites("nehrp", NshmpSite.nehrp(), DEC2_FMT);
   }
 
   static void writeNshmpSites(String name, Collection<NshmpSite> sites)
       throws IOException {
     List<NshmpSite> sortedSites = Lists.newArrayList(sites);
     Collections.sort(sortedSites, new NshmpSite.StateComparator());
-    writeSites(name, sortedSites);
+    writeSites(name, sortedSites, DEC2_FMT);
   }
 
-  static void writeSites(String name, Collection<? extends NamedLocation> sites)
-      throws IOException {
+  static void writeCybershakeSites(
+      String name,
+      Collection<CybershakeSite> sites) throws IOException {
+
+    Path jsonOut = EXPORT_DIR.resolve("sites-" + name + ".geojson");
+    writeCybershakeJsonSites(jsonOut, sites, CybershakeVs30.NONE);
+    jsonOut = EXPORT_DIR.resolve("sites-" + name + "-vs30-wills.geojson");
+    writeCybershakeJsonSites(jsonOut, sites, CybershakeVs30.WILLS);
+    jsonOut = EXPORT_DIR.resolve("sites-" + name + "-vs30-cvm.geojson");
+    writeCybershakeJsonSites(jsonOut, sites, CybershakeVs30.CVM);
+
+    Path csvOut = EXPORT_DIR.resolve("sites-" + name + ".csv");
+    writeCybershakeCsvSites(csvOut, sites, CybershakeVs30.NONE);
+    csvOut = EXPORT_DIR.resolve("sites-" + name + "-vs30-wills.csv");
+    writeCybershakeCsvSites(csvOut, sites, CybershakeVs30.WILLS);
+    csvOut = EXPORT_DIR.resolve("sites-" + name + "-vs30-cvm.csv");
+    writeCybershakeCsvSites(csvOut, sites, CybershakeVs30.CVM);
+  }
+
+  static void writeSites(
+      String name,
+      Collection<? extends NamedLocation> sites,
+      DecimalFormat latLonFormat) throws IOException {
+
     Path jsonOut = EXPORT_DIR.resolve("sites-" + name + ".geojson");
     writeJsonSites(jsonOut, sites);
     Path csvOut = EXPORT_DIR.resolve("sites-" + name + ".csv");
-    writeCsvSites(csvOut, sites);
+    writeCsvSites(csvOut, sites, latLonFormat);
   }
 
-  private static final DecimalFormat FMT = new DecimalFormat("0.00");
+  private static final DecimalFormat DEC2_FMT = new DecimalFormat("0.00");
+  private static final DecimalFormat DEC3_FMT = new DecimalFormat("0.000");
+  private static final DecimalFormat DEC5_FMT = new DecimalFormat("0.00000");
   private static final int NAME_BUFF = 28;
   private static final int LON_BUFF = 8;
   private static final int LAT_BUFF = 7;
+  private static final int LON_BUFF_CYBER = 10;
+  private static final int LAT_BUFF_CYBER = 9;
 
-  private static void writeCsvSites(Path out, Collection<? extends NamedLocation> locs)
-      throws IOException {
+  private enum CybershakeVs30 {
+    NONE,
+    WILLS,
+    CVM;
+  }
+
+  private static void writeCsvSites(
+      Path out,
+      Collection<? extends NamedLocation> locs,
+      final DecimalFormat latLonFormat) throws IOException {
+
     Iterable<String> lines = Iterables.transform(locs, new Function<NamedLocation, String>() {
       @Override
       public String apply(NamedLocation loc) {
         StringBuilder sb = new StringBuilder();
         sb.append(padEnd(loc.toString() + ",", NAME_BUFF, ' '));
-        sb.append(padStart(FMT.format(loc.location().lon()), LON_BUFF, ' '));
+        sb.append(padStart(latLonFormat.format(loc.location().lon()), LON_BUFF, ' '));
         sb.append(',');
-        sb.append(padStart(FMT.format(loc.location().lat()), LAT_BUFF, ' '));
+        sb.append(padStart(latLonFormat.format(loc.location().lat()), LAT_BUFF, ' '));
         return sb.toString();
       }
     });
@@ -266,6 +309,43 @@ final class NshmpSiteFiles {
         StandardCharsets.UTF_8);
   }
 
+  private static void writeCybershakeCsvSites(
+      Path out,
+      Collection<CybershakeSite> locs,
+      final CybershakeVs30 vs30) throws IOException {
+
+    Iterable<String> lines = Iterables.transform(locs, new Function<CybershakeSite, String>() {
+      @Override
+      public String apply(CybershakeSite loc) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(padEnd(loc.toString() + ",", NAME_BUFF, ' '));
+        sb.append(padStart(DEC5_FMT.format(loc.location().lon()), LON_BUFF_CYBER, ' '));
+        sb.append(',');
+        sb.append(padStart(DEC5_FMT.format(loc.location().lat()), LAT_BUFF_CYBER, ' '));
+        if (vs30 != CybershakeVs30.NONE) {
+          double vs30value = (vs30 == CybershakeVs30.WILLS) ? loc.willsVs30() : loc.cvmVs30();
+          sb.append(", ").append(padStart(Double.toString(vs30value), 7, ' '));
+          sb.append(", ").append(DEC2_FMT.format(loc.z1p0()));
+          sb.append(", ").append(DEC2_FMT.format(loc.z2p5()));
+        }
+        return sb.toString();
+      }
+    });
+    StringBuilder header = new StringBuilder(padEnd("name,", NAME_BUFF, ' '))
+        .append(padStart("lon", LON_BUFF_CYBER, ' '))
+        .append(',')
+        .append(padStart("lat", LAT_BUFF_CYBER, ' '));
+    if (vs30 != CybershakeVs30.NONE) {
+      header.append(",    vs30")
+          .append(", z1p0")
+          .append(", z2p5");
+    }
+    Files.write(
+        out,
+        Iterables.concat(ImmutableList.of(header.toString()), lines),
+        StandardCharsets.UTF_8);
+  }
+
   private static void writeJsonSites(Path out, Collection<? extends NamedLocation> sites)
       throws IOException {
 
@@ -277,6 +357,38 @@ final class NshmpSiteFiles {
     fc.features = features;
     String json = GeoJson.cleanPoints(GSON.toJson(fc));
     Files.write(out, json.getBytes(StandardCharsets.UTF_8));
+  }
+
+  private static void writeCybershakeJsonSites(
+      Path out,
+      Collection<CybershakeSite> sites,
+      CybershakeVs30 vs30) throws IOException {
+
+    List<Feature> features = new ArrayList<>(sites.size());
+    for (CybershakeSite loc : sites) {
+      Feature feature = GeoJson.createPoint(loc);
+      CybershakeSiteProperties props = new CybershakeSiteProperties();
+      props.title = loc.name();
+      // props.runId = loc.runId();
+      if (vs30 != CybershakeVs30.NONE) {
+        props.vs30 = (vs30 == CybershakeVs30.WILLS) ? loc.willsVs30() : loc.cvmVs30();
+        props.z1p0 = loc.z1p0();
+        props.z2p5 = loc.z2p5();
+      }
+      feature.properties = props;
+      features.add(feature);
+    }
+    FeatureCollection fc = new FeatureCollection();
+    fc.features = features;
+    String json = GeoJson.cleanPoints(GSON.toJson(fc));
+    Files.write(out, json.getBytes(StandardCharsets.UTF_8));
+  }
+
+  static class CybershakeSiteProperties extends PointProperties {
+    // Integer runId;
+    Double vs30;
+    Double z1p0;
+    Double z2p5;
   }
 
   private static final Function<NamedLocation, NamedLocation> adjustLocation_0p1() {
