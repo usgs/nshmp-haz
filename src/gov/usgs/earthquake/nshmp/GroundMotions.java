2c11f17c38c67d5dd74cb0a3a83a374a8b4c5d41
bclayton-usgs <bclayton@usgs.gov>
2018-01-19 07:34:53 -0700

    Distance is now rounded to 5 decimal points for cleaner output

--- src/gov/usgs/earthquake/nshmp/GroundMotions.java
+++ src/gov/usgs/earthquake/nshmp/GroundMotions.java
@@ -8,9 +8,9 @@ import java.util.Set;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Maps;
+import com.google.common.primitives.Doubles;
 
 import gov.usgs.earthquake.nshmp.data.Data;
-import gov.usgs.earthquake.nshmp.eq.model.Distance;
 import gov.usgs.earthquake.nshmp.gmm.Gmm;
 import gov.usgs.earthquake.nshmp.gmm.GmmInput;
 import gov.usgs.earthquake.nshmp.gmm.Imt;
@@ -32,19 +32,16 @@ public class GroundMotions {
     
     
     //.............................. Set Distance and Gmm Inputs ..........................
-    double rJB;
+    int round = 5;
+  		double rJB;
     double rX;
     double rRup;
     double rMin = 0.001;
-    double rPoints = 20.0;
+    double rPoints = 100.0;
     double rStep = (Math.log10(rMax/rMin))/(rPoints-1);
-    double[] distanceLog = Data.buildSequence(
-        Math.log10(rMin), Math.log10(rMax), rStep, true);
-    double[] distance = Data.pow10(distanceLog.clone());
+    double[] distance = Data.round(round, Data.pow10(Data.buildSequence(
+        Math.log10(rMin), Math.log10(rMax), rStep, true)));
     
-    List<Double> rJBs = new ArrayList<>();
-    List<Double> rRups = new ArrayList<>();
-    List<Double> rXs = new ArrayList<>();
     List<GmmInput> gmmInputs = new ArrayList<>(); 
     
     for(double r : distance) {
@@ -53,9 +50,6 @@ public class GroundMotions {
       rRup = Maths.hypot(r, inputModel.zTop);
       GmmInput.Builder gmmBuilder = GmmInput.builder().fromModel(inputModel);
       gmmInputs.add(gmmBuilder.distances(rJB, rRup, rX).build());
-      rJBs.add(rJB);
-      rRups.add(rRup);
-      rXs.add(rX);
     }
     //-------------------------------------------------------------------------------------
     
@@ -68,23 +62,20 @@ public class GroundMotions {
     for(Gmm gmm : gmms) {
       ImmutableList.Builder<Double> means = ImmutableList.builder();
       ImmutableList.Builder<Double> sigmas = ImmutableList.builder();
-     
+           
       for(GmmInput gmmInput : gmmInputs) {
         ScalarGroundMotion gm = gmm.instance(imt).calc(gmmInput);
         means.add(gm.mean());
-        sigmas.add(gm.sigma());      
+        sigmas.add(gm.sigma()); 
       }
       
       meanMap.put(gmm, means.build());
       sigmaMap.put(gmm, sigmas.build());
-      distanceMap.put(gmm, rXs);
+      distanceMap.put(gmm, Doubles.asList(distance));
     }
     //-------------------------------------------------------------------------------------
     
     return new DistanceResult(
-        rJBs,
-        rRups,
-        rXs,
         Maps.immutableEnumMap(distanceMap),
         Maps.immutableEnumMap(meanMap),
         Maps.immutableEnumMap(sigmaMap)
@@ -97,23 +88,14 @@ public class GroundMotions {
   //............................. Class: DistanceResult ...................................
   public static class DistanceResult {
     
-    public final List<Double> rJBs;
-    public final List<Double> rRups;  
-    public final List<Double> rXs;
     public final Map<Gmm, List<Double>> means;
     public final Map<Gmm, List<Double>> distance;
     public final Map<Gmm, List<Double>> sigmas;
     
     DistanceResult(
-        List<Double> rJBs,
-        List<Double> rRups,
-        List<Double> rXs,
         Map<Gmm, List<Double>> distance,
         Map<Gmm, List<Double>> means,
         Map<Gmm, List<Double>> sigmas){
-      this.rJBs = rJBs;
-      this.rRups = rRups;
-      this.rXs = rXs;
       this.distance = distance;    
       this.means = means;
       this.sigmas = sigmas;
