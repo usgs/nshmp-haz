f5043b9c87d9e75e08bc929177a332bb0613c5e6
bclayton-usgs <bclayton@usgs.gov>
2018-05-04 10:06:29 -0600

    New interface for all GeoJson types

---
+++ src/gov/usgs/earthquake/nshmp/json/GeoJson.java
@@ -0,0 +1,22 @@
+package gov.usgs.earthquake.nshmp.json;
+
+/**
+ * Interface for all {@link GeoJsonType}s to implement.
+ * 
+ * @author Brandon Clayton
+ */
+public interface GeoJson {
+
+  /**
+   * Return the {@link GeoJsonType} representing the {@code Geometry}.
+   * @return The {@code GeoJsonType}.
+   */
+  public GeoJsonType getType();
+
+  /**
+   * Return a {@code String} in JSON format.
+   * @return The JSON {@code String}
+   */
+  public String toJsonString();
+  
+}
