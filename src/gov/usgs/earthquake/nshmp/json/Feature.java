1da09cd4f66f9d8e43c6d7630b4bc0faab48fd42
bclayton-usgs <bclayton@usgs.gov>
2018-05-11 07:34:55 -0600

    Added Optional id field

--- src/gov/usgs/earthquake/nshmp/json/Feature.java
+++ src/gov/usgs/earthquake/nshmp/json/Feature.java
@@ -3,7 +3,9 @@ package gov.usgs.earthquake.nshmp.json;
 import java.util.List;
 import java.util.Map;
 
+import com.google.common.base.Optional;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
 
 import gov.usgs.earthquake.nshmp.geo.Location;
 import gov.usgs.earthquake.nshmp.geo.LocationList;
@@ -31,6 +33,8 @@ import static com.google.common.base.Preconditions.checkNotNull;
 public class Feature implements GeoJson {
   /** The {@link GeoJsonType} of GeoJson object: Feature */
   private final String type;
+  /** An {@code Optional} id field */
+  private JsonElement id = new JsonPrimitive("");
   /** The {@link Geometry} */
   private Geometry geometry;
   /** The {@link Properties} */
@@ -42,22 +46,28 @@ public class Feature implements GeoJson {
    * 
    * Example:
    * <pre>
+   *   Properties properties = Properties.builder().title("Golden").build();
    *   Location loc = Location.create(39.75, -105);
    *   Point point = new Geometry.Point(loc);
-   *   Properties properties = Properties.builder().title("Golden").build();
-   *   Feature feature = new Feature(properties, point);
+   *   String id = "featureId";
+   *    
+   *   Feature feature = new Feature(properties, point, Optional.of(id));
    * </pre>
    * 
    * @param properties The {@link Properties} for the {@code Feature}.
    * @param geometry The {@link geometry} for the {@code Feature}.
+   * @param id An {@code Optional} ({@code String} or {@code int})
+   *    id for the {@code Feature}.
    */
-  public Feature(Properties properties, Geometry geometry) {
+  public Feature(Properties properties, Geometry geometry, Optional<?> id) {
     checkNotNull(geometry, "Geometry cannot be null");
     checkNotNull(properties, "Properties cannot be null");
     
     this.type = GeoJsonType.FEATURE.toUpperCamelCase();
     this.geometry = geometry;
     this.properties = properties.getProperties();
+    
+    if (id.isPresent()) this.id = Util.GSON.toJsonTree(id.get()); 
   }
 
   /**
@@ -70,12 +80,15 @@ public class Feature implements GeoJson {
    *    
    * @param properties The {@link Properties}.
    * @param multiPolygon The {@code MultiPolygon}.
+   * @param featureId An {@code Optional} ({@code String} or {@code int})
+   *    id for the {@code Feature}.
    * @return The {@code Feature}.
    */
   public static Feature createMultiPolygon(
       Properties properties,
-      MultiPolygon multiPolygon) {
-    return new Feature(properties, multiPolygon);
+      MultiPolygon multiPolygon,
+      Optional<?> featureId) {
+    return new Feature(properties, multiPolygon, featureId);
   }
   
   /**
@@ -89,18 +102,21 @@ public class Feature implements GeoJson {
    *    
    * @param properties The {@link Properties}.
    * @param polygons The {@link Polygon}s.
+   * @param featureId An {@code Optional} ({@code String} or {@code int})
+   *    id for the {@code Feature}.
    * @return The {@code Feature}.
    */
   public static Feature createMultiPolygon(
       Properties properties, 
-      List<Polygon> polygons) {
+      List<Polygon> polygons,
+      Optional<?> featureId) {
     MultiPolygon.Builder builder = MultiPolygon.builder();
     
     for (Polygon polygon : polygons) {
       builder.addPolygon(polygon);
     }
     
-    return new Feature(properties, builder.build());
+    return new Feature(properties, builder.build(), featureId);
   }
   
   /**
@@ -114,20 +130,27 @@ public class Feature implements GeoJson {
    *      .title("Golden")
    *      .id("golden")
    *      .build();
-   *   Feature feature = Feature.createPoint(properties, latitude, longitude);
+   *      
+   *   String featureId = "id";
+   *    
+   *   Feature feature = Feature.createPoint(
+   *      properties, latitude, longitude, Optional.of(featureId));
    * </pre>
    * 
    * @param properties The {@code Properties} ({@link Properties}).
    * @param latitude The latitude for the point.
    * @param longitude The longitude for the point.
+   * @param featureId An {@code Optional} ({@code String} or {@code int})
+   *    id for the {@code Feature}.
    * @return A new GeoJson {@code Feature}.
    */
   public static Feature createPoint(
       Properties properties,
       double latitude,
-      double longitude) {
+      double longitude,
+      Optional<?> featureId) {
     Point point = new Point(latitude, longitude);
-    return new Feature(properties, point);
+    return new Feature(properties, point, featureId);
   }
 
   /**
@@ -138,20 +161,30 @@ public class Feature implements GeoJson {
    * Example:
    * <pre>
    *   Location loc = Location.create(39.75, -105);
+   *   
    *   Properties properties = Properties.builder()
    *      .title("Golden")
    *      .id("golden")
    *      .build();
-   *   Feature feature = Feature.createPoint(properties, loc);
+   *      
+   *   String featureId = "id";
+   *   
+   *   Feature feature = Feature.createPoint(
+   *      properties, loc, Optional.of(featureId));
    * </pre>
    * 
    * @param properties - The {@code Properties} ({@link Properties}).
    * @param loc - The {@code Location} ({@link Location}).
+   * @param featureId An {@code Optional} ({@code String} or {@code int})
+   *    id for the {@code Feature}.
    * @return A new GeoJson {@code Feature} ({@link Feature}).
    */
-  public static Feature createPoint(Properties properties, Location loc) {
+  public static Feature createPoint(
+      Properties properties, 
+      Location loc, 
+      Optional<?> featureId) {
     Point point = new Point(loc);
-    return new Feature(properties, point);
+    return new Feature(properties, point, featureId);
   }
 
   /**
@@ -173,40 +206,66 @@ public class Feature implements GeoJson {
    *      .id("golden")
    *      .build();
    *      
-   *   Feature feature = Feature.createPolygon(properties, border);
+   *   String id = "id";
+   *      
+   *   Feature feature = Feature.createPolygon(
+   *      properties, Optional.of(featureId), border);
    * </pre>
    * 
    * @param properties The {@code Properties} 
+   * @param featureId An {@code Optional} ({@code String} or {@code int}) 
+   *    id for the {@code Feature}.
    * @param border The border for the {@code Polygon}
    * @param interiors The interiors for the {@code Polygon}  
    * @return A new GeoJson {@code Feature} 
    */
   public static Feature createPolygon(
       Properties properties,
+      Optional<?> featureId,
       LocationList border,
       LocationList... interiors) {
     Polygon polygon = new Polygon(border, interiors);
-    return new Feature(properties, polygon);
+    return new Feature(properties, polygon, featureId);
   }
 
   /**
    * Return the {@link Geometry}.
    * <br>
    * 
-   * It is best to cast to the specific {@code Geometry} type;
-   * <br><br>
+   * It is best to get the specific {@code Geometry} by using:
+   *    <ul>
+   *      <li> {@link Geometry#asMultiPolygon()} </li>
+   *      <li> {@link Geometry#asPoint()} </li>
+   *      <li> {@link Geometry#asPolygon()} </li>
+   *    </ul> 
    * 
    * Example:
    * <pre>
    *  Feature feature = Feature.createPoint(Properties, 40, -120);
    *  
-   *  Point pointGeom = (Point) feature.getGeometry();
+   *  Point pointGeom = feature.getGeometry().asPoint();
    * </pre>
    * 
    * @return The {@code Geometry}.
    */
   public Geometry getGeometry() {
-    return this.geometry;
+    return geometry;
+  }
+ 
+  /**
+   * Return the {@code Optional} {@code Feature} id field as a {@code String}.
+   * @return The {@code Feature} id.
+   */
+  public String getId() {
+    return id.getAsString();
+  }
+  
+  /**
+   * Return the {@code Optional} {@code Feature} id field as an {@code int}.
+   * @return The {@code Feature} id.
+   */
+  public int getNumericId() {
+    return id.getAsInt();
   }
   
   /**
@@ -214,7 +273,7 @@ public class Feature implements GeoJson {
    * @return The {@code Properties}
    */
   public Properties getProperties() {
-    return Properties.builder().putAll(this.properties).build();
+    return Properties.builder().putAll(properties).build();
   }
  
   @Override
@@ -223,7 +282,7 @@ public class Feature implements GeoJson {
    * @return The {@code GeoJsonType}.
    */
   public GeoJsonType getType() {
-    return GeoJsonType.getEnum(this.type);
+    return GeoJsonType.getEnum(type);
   }
  
   @Override
