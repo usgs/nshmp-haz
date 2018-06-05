718799ff7380cbf3a2f1990bfaf6b3bd52c63ad6
bclayton-usgs <bclayton@usgs.gov>
2018-06-05 13:40:20 -0600

    Replaced static factory methods with a builder

--- src/gov/usgs/earthquake/nshmp/json/Feature.java
+++ src/gov/usgs/earthquake/nshmp/json/Feature.java
@@ -1,32 +1,18 @@
 package gov.usgs.earthquake.nshmp.json;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 import com.google.gson.JsonElement;
-import com.google.gson.JsonPrimitive;
 
 import gov.usgs.earthquake.nshmp.geo.Location;
 import gov.usgs.earthquake.nshmp.geo.LocationList;
 
+import static com.google.common.base.Preconditions.checkState;
+
 /**
  * Create a GeoJson {@code Feature} with a {@link Geometry} and
- *    {@link Properties}. See {@link #Feature(Properties, Geometry)} for an
- *    example. 
- * <br><br>
- * 
- * Static factory classes are provided to create a {@link Point} and a
- *    {@link Polygon} (see each for examples): 
- *  <ul> 
- *    <li> {@link Feature#createPoint(Properties, Location)} </li> 
- *    <li> {@link Feature#createPoint(Properties, double, double)} </li> 
- *    <li> {@link Feature#createPolygon(Properties, LocationList, LocationList...)} </li> 
- *    <li> {@link Feature#createMultiPolygon(Properties, List)} </li>
- *    <li> {@link Feature#createMultiPolygon(Properties, MultiPolygon)} </li>
- *  </ul>
+ *    {@link Properties}. 
  * 
  * @author Brandon Clayton
  */
@@ -34,198 +20,32 @@ public class Feature implements GeoJson {
   /** The {@link GeoJsonType} of GeoJson object: Feature */
   private final String type;
   /** An {@code Optional} id field */
-  private JsonElement id = null; 
+  private JsonElement id;
+  /** {@code Feature} bounding box */
+  private double[] bbox;
   /** The {@link Geometry} */
   private Geometry geometry;
   /** The {@link Properties} */
   private Map<String, JsonElement> properties;
 
-  /**
-   * Create a new instance of a {@code Feature}. 
-   * <br><br>
-   * 
-   * Example:
-   * <pre>
-   *   Properties properties = Properties.builder().title("Golden").build();
-   *   Location loc = Location.create(39.75, -105);
-   *   Point point = new Geometry.Point(loc);
-   *   String id = "featureId";
-   *    
-   *   Feature feature = new Feature(properties, point, Optional.of(id));
-   * </pre>
-   * 
-   * @param properties The {@link Properties} for the {@code Feature}.
-   * @param geometry The {@link geometry} for the {@code Feature}.
-   * @param id An {@code Optional} ({@code String} or {@code int})
-   *    id for the {@code Feature}.
-   */
-  public Feature(Properties properties, Geometry geometry, Optional<?> id) {
-    checkNotNull(geometry, "Geometry cannot be null");
-    checkNotNull(properties, "Properties cannot be null");
-    
+  private Feature(Builder builder) {
     this.type = GeoJsonType.FEATURE.toUpperCamelCase();
-    this.geometry = geometry;
-    this.properties = properties.getProperties();
-    
-    if (id.isPresent()) this.id = Util.GSON.toJsonTree(id.get()); 
+    this.id = builder.id;
+    this.bbox = builder.bbox;
+    this.geometry = builder.geometry;
+    this.properties = builder.properties;
   }
 
   /**
-   * Static factory method to create a {@link Feature} with a 
-   *    {@link MultiPolygon} {@link Geometry}.
+   * Return a {@code double[]} representing the bounding
+   *    box for the {@code Feature}. 
    * <br>
    * 
-   * See {@link MultiPolygon.Builder} for an example on creating a 
-   *    {@code MultiPolygon}. 
-   *    
-   * @param properties The {@link Properties}.
-   * @param multiPolygon The {@code MultiPolygon}.
-   * @param featureId An {@code Optional} ({@code String} or {@code int})
-   *    id for the {@code Feature}.
-   * @return The {@code Feature}.
+   * If not bounding box was set, returns {@code null}.
+   * @return The bounding box.
    */
-  public static Feature createMultiPolygon(
-      Properties properties,
-      MultiPolygon multiPolygon,
-      Optional<?> featureId) {
-    return new Feature(properties, multiPolygon, featureId);
-  }
-  
-  /**
-   * Static factory method to create a {@link Feature} with a 
-   *    {@link MultiPolygon} {@link Geometry} given a 
-   *    {@code List<Polygon>}.
-   * <br>
-   * 
-   * See {@link MultiPolygon.Builder} for an example on creating a 
-   *    {@code MultiPolygon}. 
-   *    
-   * @param properties The {@link Properties}.
-   * @param polygons The {@link Polygon}s.
-   * @param featureId An {@code Optional} ({@code String} or {@code int})
-   *    id for the {@code Feature}.
-   * @return The {@code Feature}.
-   */
-  public static Feature createMultiPolygon(
-      Properties properties, 
-      List<Polygon> polygons,
-      Optional<?> featureId) {
-    MultiPolygon.Builder builder = MultiPolygon.builder();
-    
-    for (Polygon polygon : polygons) {
-      builder.addPolygon(polygon);
-    }
-    
-    return new Feature(properties, builder.build(), featureId);
-  }
-  
-  /**
-   * Static factory method to create a {@code Feature} with a
-   *    {@link Point} {@link Geometry} given a latitude and longitude in degrees. 
-   * <br><br> 
-   * 
-   * Example:
-   * <pre>
-   *   Properties properties = Properties.builder()
-   *      .title("Golden")
-   *      .id("golden")
-   *      .build();
-   *      
-   *   String featureId = "id";
-   *    
-   *   Feature feature = Feature.createPoint(
-   *      properties, latitude, longitude, Optional.of(featureId));
-   * </pre>
-   * 
-   * @param properties The {@code Properties} ({@link Properties}).
-   * @param latitude The latitude for the point.
-   * @param longitude The longitude for the point.
-   * @param featureId An {@code Optional} ({@code String} or {@code int})
-   *    id for the {@code Feature}.
-   * @return A new GeoJson {@code Feature}.
-   */
-  public static Feature createPoint(
-      Properties properties,
-      double latitude,
-      double longitude,
-      Optional<?> featureId) {
-    Point point = new Point(latitude, longitude);
-    return new Feature(properties, point, featureId);
-  }
-
-  /**
-   * Static factory method to create a {@code Feature} with a
-   *    {@link Point} {@link Geometry} given a {@code Location}. 
-   * <br><br>
-   * 
-   * Example:
-   * <pre>
-   *   Location loc = Location.create(39.75, -105);
-   *   
-   *   Properties properties = Properties.builder()
-   *      .title("Golden")
-   *      .id("golden")
-   *      .build();
-   *      
-   *   String featureId = "id";
-   *   
-   *   Feature feature = Feature.createPoint(
-   *      properties, loc, Optional.of(featureId));
-   * </pre>
-   * 
-   * @param properties - The {@code Properties} ({@link Properties}).
-   * @param loc - The {@code Location} ({@link Location}).
-   * @param featureId An {@code Optional} ({@code String} or {@code int})
-   *    id for the {@code Feature}.
-   * @return A new GeoJson {@code Feature} ({@link Feature}).
-   */
-  public static Feature createPoint(
-      Properties properties, 
-      Location loc, 
-      Optional<?> featureId) {
-    Point point = new Point(loc);
-    return new Feature(properties, point, featureId);
-  }
-
-  /**
-   * Static factory method to create a {@code Feature} with a
-   *    {@link Polygon} {@link Geometry} given a {@link LocationList}.
-   * <br><br> 
-   * 
-   * Example:
-   * <pre>
-   *   LocationList border = LocationList.builder()
-   *       .add(40, -120)
-   *       .add(38, -120)
-   *       .add(38, -122)
-   *       .add(40, -120)
-   *       .build();
-   *       
-   *   Properties properties = Properties.builder()
-   *      .title("Golden")
-   *      .id("golden")
-   *      .build();
-   *      
-   *   String id = "id";
-   *      
-   *   Feature feature = Feature.createPolygon(
-   *      properties, Optional.of(featureId), border);
-   * </pre>
-   * 
-   * @param properties The {@code Properties} 
-   * @param featureId An {@code Optional} ({@code String} or {@code int}) 
-   *    id for the {@code Feature}.
-   * @param border The border for the {@code Polygon}
-   * @param interiors The interiors for the {@code Polygon}  
-   * @return A new GeoJson {@code Feature} 
-   */
-  public static Feature createPolygon(
-      Properties properties,
-      Optional<?> featureId,
-      LocationList border,
-      LocationList... interiors) {
-    Polygon polygon = new Polygon(border, interiors);
-    return new Feature(properties, polygon, featureId);
+  public double[] getBbox() {
+    return bbox != null ? bbox : null;
   }
 
   /**
@@ -290,7 +110,7 @@ public class Feature implements GeoJson {
    * Return a {@code String} in JSON format.
    */
   public String toJsonString() {
-    return Util.cleanPoly(Util.GSON.toJson(this, Feature.class));
+    return JsonUtil.cleanPoly(JsonUtil.GSON.toJson(this, Feature.class));
   }
   
   /**
@@ -301,4 +121,207 @@ public class Feature implements GeoJson {
     public static final String EXTENTS = "Extents";
   }
 
+  /**
+   * Return a new instance of {@link Builder}.
+   * @return New {@code Builder}.
+   */
+  public static Builder builder() {
+    return new Builder();
+  }
+  
+  /**
+   * Build a {@code Feature} with a {@link Geometry},
+   *    {@link Properties}, an {@code Optional} id, and 
+   *    and {@code Optional} bounding box.
+   * <br><br>
+   *    
+   * Example:
+   * <pre>
+   *  // Build properties 
+   *  Properties properties = Properties.builder()
+   *      .title("Golden")
+   *      .id("golden")
+   *      .build();
+   *       
+   *  // Build a Feature
+   *  Feature feature = Feature.builder()
+   *      .addPoint(39.75, -105)
+   *      .properties(properties)
+   *      .build();
+   * 
+   * </pre>
+   * @author Brandon Clayton
+   */
+  public static class Builder {
+    private double[] bbox;
+    private JsonElement id; 
+    private Geometry geometry;
+    private Map<String, JsonElement> properties;
+  
+    private Builder() {}
+   
+    /**
+     * Return a new {@code Feature}.
+     * @return The {@code Feature}.
+     */
+    public Feature build() {
+      checkState(geometry != null, "Geometry must be set");
+      checkState(properties != null, "Properties must be set");
+      return new Feature(this);
+    }
+   
+    /**
+     * Set the {@code Feature} {@code Geometry}. 
+     * @param geometry The {@code Geometry}.
+     * @return The {@code Builder}.
+     */
+    public Builder addGeometry(Geometry geometry) {
+      this.geometry = geometry;
+      return this;
+    }
+
+    /**
+     * Set the {@code Feature} {@code Geometry} with a {@code MultiPolygon}. 
+     * @param multiPolygon The {@code MultiPolygon}.
+     * @return The {@code Builder}.
+     */
+    public Builder addMultiPolygon(MultiPolygon multiPolygon) {
+      checkGeometry();
+      geometry = multiPolygon;
+      return this;
+    }
+    
+    /**
+     * Set the {@code Feature} {@code Geometry} with a {@code MultiPolygon}. 
+     * @param polygons The {@code List<Polygon>}. 
+     * @return The {@code Builder}.
+     */
+    public Builder addMultiPolygon(List<Polygon> polygons) {
+      checkGeometry();
+      MultiPolygon.Builder multiPolygon = MultiPolygon.builder();
+      polygons.stream().forEach(polygon -> multiPolygon.addPolygon(polygon));
+      geometry = multiPolygon.build();
+      return this;
+    }
+    
+    /**
+     * Set the {@code Feature} {@code Geometry} with a {@code Point}. 
+     * @param point The {@code Point}. 
+     * @return The {@code Builder}.
+     */
+    public Builder addPoint(Point point) {
+      checkGeometry();
+      geometry = point;
+      return this;
+    }
+    
+    /**
+     * Set the {@code Feature} {@code Geometry} with a {@code Point}. 
+     * @param loc The {@code Location} for the {@code Point}. 
+     * @return The {@code Builder}.
+     */
+    public Builder addPoint(Location loc) {
+      checkGeometry();
+      geometry = new Point(loc);
+      return this;
+    }
+    
+    /**
+     * Set the {@code Feature} {@code Geometry} with a {@code Point}. 
+     * @param latitude The latitude of the {@code Point}. 
+     * @param longitude The longitude of the {@code Point}. 
+     * @return The {@code Builder}.
+     */
+    public Builder addPoint(double latitude, double longitude) {
+      checkGeometry();
+      geometry = new Point(latitude, longitude);
+      return this;
+    }
+    
+    /**
+     * Set the {@code Feature} {@code Geometry} with a {@code Polygon}. 
+     * @param polygon The {@code Polygon}. 
+     * @return The {@code Builder}.
+     */
+    public Builder addPolygon(Polygon polygon) {
+      checkGeometry();
+      geometry = polygon;
+      return this;
+    }
+    
+    /**
+     * Set the {@code Feature} {@code Geometry} with a {@code Polygon}. 
+     * @param border The {@code Polygon} border.
+     * @param interiors The {@code Polygon} interiors.
+     * @return The {@code Builder}.
+     */
+    public Builder addPolygon(LocationList border, LocationList... interiors) {
+      checkGeometry();
+      geometry = new Polygon(border, interiors); 
+      return this;
+    }
+   
+    /**
+     * Set the {@code Feature} bounding box.
+     * <br>
+     * This is an optional field and does not need set.
+     * @param bbox The bounding box.
+     * @return The {@code Builder}.
+     */
+    public Builder bbox(double[] bbox) {
+      this.bbox = bbox;
+      return this;
+    }
+    
+    /**
+     * Set the {@code Feature} id;
+     * <br>
+     * This is an optional field and does not need set.
+     * @param id The id.
+     * @return The {@code Builder}.
+     */
+    public Builder id(int id) {
+      this.id = JsonUtil.GSON.toJsonTree(id, Integer.class);
+      return this;
+    }
+    
+    /**
+     * Set the {@code Feature} id;
+     * <br>
+     * This is an optional field and does not need set.
+     * @param id The id.
+     * @return The {@code Builder}.
+     */
+    public Builder id(JsonElement id) {
+      this.id = id;
+      return this;
+    }
+    
+    /**
+     * Set the {@code Feature} id;
+     * <br>
+     * This is an optional field and does not need set.
+     * @param id The id.
+     * @return The {@code Builder}.
+     */
+    public Builder id(String id) {
+      this.id = JsonUtil.GSON.toJsonTree(id, String.class);
+      return this;
+    }
+   
+    /**
+     * Set the {@code Feature} {@link Properties}.
+     * @param properties The {@code Properties}.
+     * @return The {@code Builder}.
+     */
+    public Builder properties(Properties properties) {
+      this.properties = properties.getProperties();
+      return this;
+    }
+    
+    private void checkGeometry() {
+      checkState(geometry == null, "Geometry already set");
+    }
+  }
+ 
 }
