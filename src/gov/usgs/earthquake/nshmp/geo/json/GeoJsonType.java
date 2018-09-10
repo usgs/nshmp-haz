18340b24283a61ceab088a331208178878837613
Peter Powers <pmpowers@usgs.gov>
2018-09-10 10:18:57 -0600

    enum string converter and tmp serializer

--- src/gov/usgs/earthquake/nshmp/geo/json/GeoJsonType.java
+++ src/gov/usgs/earthquake/nshmp/geo/json/GeoJsonType.java
@@ -2,6 +2,16 @@ package gov.usgs.earthquake.nshmp.geo.json;
 
 import static com.google.common.base.CaseFormat.UPPER_CAMEL;
 import static com.google.common.base.CaseFormat.UPPER_UNDERSCORE;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.lang.reflect.Type;
+
+import com.google.common.base.CaseFormat;
+import com.google.common.base.Converter;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
 
 /**
  * The nine case-sensitive GeoJson types: 
@@ -54,6 +64,56 @@ public enum GeoJsonType {
       }
     }
     throw new IllegalStateException("Could not find type: " + type);
+  }
+
+  /**
+   * A converter for use with enums whose serialized form is a
+   * {@link CaseFormat} other than {@code UPPER_UNDERSCORE}. This method assumes
+   * the values of any supplied enum are declared using UPPER_UNDERSCORE;
+   * results are not guaraneteed if they are not.
+   */
+  public static <E extends Enum<E>> Converter<E, String> enumStringConverter(
+      Class<E> enumType,
+      CaseFormat toStringFormat) {
+
+    return new EnumStringConverter<>(
+        checkNotNull(enumType),
+        checkNotNull(toStringFormat));
+  }
+
+  static final class EnumStringConverter<E extends Enum<E>> extends Converter<E, String> {
+
+    final Class<E> enumType;
+    final CaseFormat enumFormat = CaseFormat.UPPER_UNDERSCORE;
+    final CaseFormat stringFormat;
+
+    private EnumStringConverter(Class<E> enumType, CaseFormat stringFormat) {
+      this.enumType = enumType;
+      this.stringFormat = stringFormat;
+    }
+
+    @Override
+    protected String doForward(E e) {
+      return enumFormat.to(stringFormat, e.name());
+    }
+
+    @Override
+    protected E doBackward(String s) {
+      return Enum.valueOf(enumType, stringFormat.to(enumFormat, s));
+    }
+  }
+
+  /* Enum.toString() serializer. */
+  static final class Serializer<E extends Enum<E>> implements JsonSerializer<E> {
+
+    @Override
+    public JsonElement serialize(
+        E src,
+        Type typeOfSrc,
+        JsonSerializationContext context) {
+
+      return new JsonPrimitive(src.toString());
+    }
   }
 
 }
