76b23c84502ccb41539ea252fbec9734c9486af8
bclayton-usgs <bclayton@usgs.gov>
2018-11-14 08:24:07 -0700

    Cleanup

--- src/gov/usgs/earthquake/nshmp/tree/RegularBranch.java
+++ src/gov/usgs/earthquake/nshmp/tree/RegularBranch.java
@@ -1,19 +1,23 @@
 package gov.usgs.earthquake.nshmp.tree;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import gov.usgs.earthquake.nshmp.data.Data;
+
 /**
  * Basic logic tree branch implementation.
  *
  * @author Brandon Clayton
  */
-class RegularBranch<T> implements Branch<T> {
+public class RegularBranch<T> implements Branch<T> {
   private final String id;
   private final double weight;
   private final T value;
 
   RegularBranch(String id, double weight, T value) {
-    this.id = id;
-    this.weight = weight;
-    this.value = value;
+    this.id = checkNotNull(id);
+    this.weight = Data.checkWeight(weight);
+    this.value = checkNotNull(value);
   }
 
   @Override
--- src/gov/usgs/earthquake/nshmp/tree/RegularLogicTree.java
+++ src/gov/usgs/earthquake/nshmp/tree/RegularLogicTree.java
@@ -11,7 +11,7 @@ import com.google.common.collect.ImmutableList;
  * @author Brandon Clayton
  * @author Peter Powers
  */
-class RegularLogicTree<T> implements LogicTree<T> {
+public class RegularLogicTree<T> implements LogicTree<T> {
 
   private final List<Branch<T>> branches;
   private final double[] cumulativeWeights;
@@ -41,32 +41,9 @@ class RegularLogicTree<T> implements LogicTree<T> {
     return samples.build();
   }
 
-  /*
-   * Override the iterator to ensure a pointer to the branch is returned and not
-   * a copy of the branch.
-   */
   @Override
   public Iterator<Branch<T>> iterator() {
-    return new Iterator<Branch<T>>() {
-
-      private int index = 0;
-      private int indexEnd = branches.size();
-
-      @Override
-      public boolean hasNext() {
-        return index < indexEnd;
-      }
-
-      @Override
-      public Branch<T> next() {
-        return branches.get(index++);
-      }
-
-      @Override
-      public void remove() {
-        throw new UnsupportedOperationException();
-      }
-    };
+    return branches.iterator(); 
   }
 
 }
