ca68983ce95e98a09c9308ed8d33b7cd0a891ef4
Peter Powers <pmpowers@usgs.gov>
2019-02-06 21:48:49 -0700

    exposed config threadCount

--- src/gov/usgs/earthquake/nshmp/calc/CalcConfig.java
+++ src/gov/usgs/earthquake/nshmp/calc/CalcConfig.java
@@ -1391,6 +1391,16 @@ public final class CalcConfig {
      */
 
     /**
+     * Set the {@link ThreadCount}.
+     * 
+     * @see gov.usgs.earthquake.nshmp.calc.CalcConfig.Performance#threadCount
+     */
+    public Builder threadCount(ThreadCount threadCount) {
+      this.performance.threadCount = checkNotNull(threadCount);
+      return this;
+    }
+    
+    /**
      * Set the IMTs for which results should be calculated.
      * 
      * @see gov.usgs.earthquake.nshmp.calc.CalcConfig.Hazard#imts
