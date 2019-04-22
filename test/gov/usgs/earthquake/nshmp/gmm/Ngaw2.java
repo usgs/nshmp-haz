3f2b125bf4c508f920c3f079bd543ddea38f3b8b
bclayton-usgs <bclayton@usgs.gov>
2019-04-22 08:39:34 -0600

    loadInputs done in GmmTest

--- test/gov/usgs/earthquake/nshmp/gmm/Ngaw2.java
+++ test/gov/usgs/earthquake/nshmp/gmm/Ngaw2.java
@@ -27,23 +27,13 @@ public class Ngaw2 extends GmmTest {
   private static String GMM_INPUTS = "NGA_inputs.csv";
   private static String GMM_RESULTS = "NGAW2_results.csv";
 
-  static {
-    try {
-      inputsList = loadInputs(GMM_INPUTS);
-    } catch (IOException ioe) {
-      ioe.printStackTrace();
-      System.exit(1);
-    }
-  }
-
   @Parameters(name = "{index}: {0} {2} {1}")
-  public static Collection<Object[]> data()
-      throws IOException {
+  public static Collection<Object[]> data() throws IOException {
     return loadResults(GMM_RESULTS);
   }
 
   public Ngaw2(int index, Gmm gmm, Imt imt, double exMedian, double exSigma) {
-    super(index, gmm, imt, exMedian, exSigma);
+    super(index, gmm, imt, exMedian, exSigma, GMM_INPUTS);
   }
 
   /* Result generation sets */
