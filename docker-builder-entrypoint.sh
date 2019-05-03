3baded6ab583e6c3b16d363aa3d18a0ed36ed9b7
bclayton-usgs <bclayton@usgs.gov>
2019-05-03 17:48:41 -0600

    format update

--- docker-builder-entrypoint.sh
+++ docker-builder-entrypoint.sh
@@ -16,23 +16,23 @@
 #   None
 ####
 download_repo() {
-  local repo=${1}
-  local version=${2}
-  local url="https://github.com/usgs/${repo}/archive/${version}.tar.gz"
+  local repo=${1};
+  local version=${2};
+  local url="https://github.com/usgs/${repo}/archive/${version}.tar.gz";
 
-  printf "\n Downloading [${url}] \n"
-  curl -L ${url} | tar -xz
-  mv ${repo}-${version#v*} ${repo}
+  printf "\n Downloading [${url}] \n";
+  curl -L ${url} | tar -xz;
+  mv ${repo}-${version#v*} ${repo};
 }
 
 # Download nshm-ak-2007
-download_repo "nshm-ak-2007" ${NSHM_AK_2007_VERSION}
+download_repo "nshm-ak-2007" ${NSHM_AK_2007_VERSION};
 
 # Download nshm-cous-2008
-download_repo "nshm-cous-2008" ${NSHM_COUS_2008_VERSION}
+download_repo "nshm-cous-2008" ${NSHM_COUS_2008_VERSION};
 
 # Download nshm-cous-2014
-download_repo "nshm-cous-2014" ${NSHM_COUS_2014_VERSION}
+download_repo "nshm-cous-2014" ${NSHM_COUS_2014_VERSION};
 
 # Download nshm-cous-2018
-download_repo "nshm-cous-2018" ${NSHM_COUS_2018_VERSION}
+download_repo "nshm-cous-2018" ${NSHM_COUS_2018_VERSION};
