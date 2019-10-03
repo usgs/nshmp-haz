e501543408e3d7e03826b133f6549ff06857b3d4
Brandon Clayton <bclayton@usgs.gov>
2019-10-03 16:34:46 -0600

    Revert "Migrate nshmp-haz-ws"

--- src/gov/usgs/earthquake/nshmp/www/meta/Constraints.java
+++
@@ -1,7 +0,0 @@
-package gov.usgs.earthquake.nshmp.www.meta;
-
-/**
- * Marker interface for supported parameter lists and ; individual enums provide
- * concrete implementations.
- */
-public interface Constraints {}
