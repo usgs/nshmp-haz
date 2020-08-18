5a2ba5cdfe688e85f8c3d2e280ecd8e36ef8c9e5
bclayton-usgs <bclayton@usgs.gov>
2020-08-18 09:09:58 -0600

    add function

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,9 +7,6 @@
 set -o errexit;
 set -o errtrace;
 
-# Import bash functions from usgsnshmp/centos
-. ${BASH_FUNCTIONS}
-
 # check_config_file global return variable
 CHECK_CONFIG_FILE_RETURN="";
 
@@ -174,6 +171,44 @@ check_sites_file() {
 
   # Return
   CHECK_SITE_FILE_RETURN=${site_file};
+}
+
+####
+# Download a repository from Github.
+# Globals:
+#   (string) LOG_FILE - The log file
+# Arguments:
+#   (string) user - The Github user
+#   (string) repo - The project to download
+#   (string) version - The version to download
+#   (string) directory - The direcotry name for repo download
+# Returns:
+#   None
+####
+download_repo() {
+  local usage="download_repo <user> <repo> <version>";
+
+  local user=${1};
+  local repo=${2};
+  local version=${3};
+  local directory=${4};
+  local url="https://github.com/${user}/${repo}/archive/${version}.tar.gz";
+
+  if [ ${version} == "null" ]; then
+    printf "\n Skipping download of [${user}/${repo}]\n";
+    return;
+  fi
+
+  printf "\n Downloading [${url}] \n\n";
+
+  if [ -z "${directory}" ]; then
+    directory=${repo};
+  fi
+
+  curl -L ${url} | tar -xz 2> ${LOG_FILE} || \
+      error_exit "Could not download [${url}]" "$(< ${LOG_FILE})" "${usage}";
+
+  mv ${repo}-${version#v*} ${directory};
 }
 
 ####
