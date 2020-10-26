9dad1028e3e9474ea57fdd8731b8dcb960e96ab0
Peter Powers <pmpowers@usgs.gov>
2020-10-26 12:03:22 -0600

    updated wong coeffs, gmm ids and 760 implementation

--- src/gov/usgs/earthquake/nshmp/gmm/WongEtAl_2015.java
+++ src/gov/usgs/earthquake/nshmp/gmm/WongEtAl_2015.java
@@ -14,7 +14,6 @@ import static gov.usgs.earthquake.nshmp.gmm.Imt.SA5P0;
 import static gov.usgs.earthquake.nshmp.gmm.Imt.SA7P5;
 import static java.lang.Math.exp;
 import static java.lang.Math.log;
-import static java.lang.Math.max;
 
 import java.util.Map;
 
@@ -40,6 +39,14 @@ import gov.usgs.earthquake.nshmp.gmm.GmmInput.Constraints;
  * coefficients for 0.075 s. Spectral periods 4 s and 7.5 s are missing
  * coefficients and are also interpolated using adjacent periods.
  *
+ * <p><b>Implementation update:</b> (October, 2020) Wong et al. provided the
+ * USGS with new coefficients for VS30=760 m/s. These have been implemented as
+ * separate model; the original model is now referenced as
+ * {@link Gmm#WONG_15_428} and the updated version {@link Gmm#WONG_15_760}. In
+ * addition, following discussion with the model devlopers, {@code WONG_15_760}
+ * uses a fixed sigma of 0.8. The {@code WONG_15_760} model continues to use
+ * {@link BooreEtAl_2014} site effect scaling for Vs30 values other than 760.
+ *
  * <p><b>Note:</b> Direct instantiation of {@code GroundMotionModel}s is
  * prohibited. Use {@link Gmm#instance(Imt)} to retrieve an instance for a
  * desired {@link Imt}.
@@ -54,23 +61,20 @@ import gov.usgs.earthquake.nshmp.gmm.GmmInput.Constraints;
  * <p><b>Component:</b> average horizontal
  *
  * @author Peter Powers
- * @see Gmm#WONG_15
+ * @see Gmm#WONG_15_428
+ * @see Gmm#WONG_15_760
  */
 @Beta
-public final class WongEtAl_2015 implements GroundMotionModel {
+public abstract class WongEtAl_2015 implements GroundMotionModel {
 
   static final String NAME = "Wong et al. (2015)";
 
-  // TODO
   static final Constraints CONSTRAINTS = Constraints.builder()
       .set(MW, Range.closed(5.0, 8.0))
       .set(RRUP, Range.closed(0.0, 300.0))
       .set(ZTOP, Range.closed(20.0, 60.0))
       .set(VS30, Range.closed(180.0, 1300.0))
       .build();
-
-  private static final double R_MIN = 20.0;
-  private static final double V_REF = 428.0;
 
   private static final Map<Imt, Range<Imt>> INTERPOLATED_IMTS = Maps.immutableEnumMap(
       ImmutableMap.of(
@@ -82,13 +86,18 @@ public final class WongEtAl_2015 implements GroundMotionModel {
    * Developer note: c6 @ 1.995Hz (0.5s) missing negative sign per email from
    * Ivan on 07/23/2019.
    */
-  static final CoefficientContainer COEFFS = new CoefficientContainer("Wong15.csv");
+  static final CoefficientContainer COEFFS_428 = new CoefficientContainer("Wong15-428.csv");
+  static final CoefficientContainer COEFFS_760 = new CoefficientContainer("Wong15-760.csv");
 
   private static final class Coefficients {
 
+    final Imt imt;
+    final double vRef;
     final double c1, c2, c3, c4, c5, c6, σ;
 
-    Coefficients(Imt imt, CoefficientContainer cc) {
+    Coefficients(Imt imt, double vRef, CoefficientContainer cc) {
+      this.imt = imt;
+      this.vRef = vRef;
       Map<String, Double> coeffs = cc.get(imt);
       c1 = coeffs.get("C1");
       c2 = coeffs.get("C2");
@@ -101,17 +110,18 @@ public final class WongEtAl_2015 implements GroundMotionModel {
   }
 
   private final Coefficients coeffs;
-  private final BooreEtAl_2014 delegate;
+  private final BooreEtAl_2014 siteDelegate;
 
   private final boolean interpolated;
   private final GroundMotionModel interpolatedGmm;
 
-  WongEtAl_2015(final Imt imt) {
-    coeffs = new Coefficients(imt, COEFFS);
-    delegate = (BooreEtAl_2014) Gmm.BSSA_14.instance(imt);
+  WongEtAl_2015(Coefficients coeffs, Gmm subtype) {
+    this.coeffs = coeffs;
+    Imt imt = coeffs.imt;
+    siteDelegate = (BooreEtAl_2014) Gmm.BSSA_14.instance(imt);
     interpolated = INTERPOLATED_IMTS.containsKey(imt);
     interpolatedGmm = interpolated
-        ? new InterpolatedGmm(Gmm.WONG_15, imt, INTERPOLATED_IMTS.get(imt))
+        ? new InterpolatedGmm(subtype, imt, INTERPOLATED_IMTS.get(imt))
         : null;
   }
 
@@ -122,9 +132,15 @@ public final class WongEtAl_2015 implements GroundMotionModel {
       return interpolatedGmm.calc(in);
     }
 
-    GmmInput in428 = GmmInput.builder().fromCopy(in).vs30(V_REF).build();
     double μ = calcMean(coeffs, in);
-    double site = delegate.calc(in).mean() - delegate.calc(in428).mean();
+    double site = 0.0;
+    if (in.vs30 != coeffs.vRef) {
+      GmmInput inVsRef = GmmInput.builder().fromCopy(in)
+          .vs30(coeffs.vRef)
+          .build();
+      site = siteDelegate.calc(in).mean() - siteDelegate.calc(inVsRef).mean();
+    }
+
     return DefaultScalarGroundMotion.create(
         μ + site,
         coeffs.σ);
@@ -133,7 +149,23 @@ public final class WongEtAl_2015 implements GroundMotionModel {
   private static final double calcMean(final Coefficients c, final GmmInput in) {
     double Mw = in.Mw;
     return c.c1 + c.c2 * Mw +
-        (c.c4 + c.c5 * Mw) * log(max(R_MIN, in.rJB) + exp(c.c3)) +
+        (c.c4 + c.c5 * Mw) * log(in.rJB + exp(c.c3)) +
         c.c6 * (Mw - 6.0) * (Mw - 6.0);
+  }
+
+  static final class Site428 extends WongEtAl_2015 {
+    static final String NAME = WongEtAl_2015.NAME + " : 428 m/s";
+
+    Site428(Imt imt) {
+      super(new Coefficients(imt, 428.0, COEFFS_428), Gmm.WONG_15_428);
+    }
+  }
+
+  static final class Site760 extends WongEtAl_2015 {
+    static final String NAME = WongEtAl_2015.NAME + " : 760 m/s";
+
+    Site760(Imt imt) {
+      super(new Coefficients(imt, 760.0, COEFFS_760), Gmm.WONG_15_760);
+    }
   }
 }
